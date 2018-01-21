6e48fb954ad6dc5e2a093f61982be4a9340bd841
Stephen Hitchner <git@stephenhitchner.com>
2018-01-21 01:47:17 -0800

    moving common components

--- src/app/App.js
+++ src/app/App.js
@@ -577,63 +577,6 @@ class FragranceRatioInput extends Component {
 	}
 }
 
-class UnitsInput extends Component {
-
-	handleChange = (e) => {
-		this.props.onChange(e.target.value);
-	}
-
-	render() {
-		return (
-			<InputGroup>
-      			<FormControl
-					type="text"
-					bsSize="sm"
-					placeholder={this.props.placeholder}
-			 		onChange={this.handleChange}
-					defaultValue={this.props.value} />
-				<InputGroup.Addon>{this.props.units}</InputGroup.Addon>
-			</InputGroup>
-		)
-	}
-}
-
-class PercentageInput extends Component {
-  
-	handleChange = (e) => {
-		this.props.onChange(e.target.value/100);
-	}
-
-	/*
-	getValidationState () {
-		const value = this.state.value;
-		if (value > 0 && value <= 1 {
-			return 'success';
-		}
-    	else if (value > 5) {
-			return 'warning';
-		}
-    	else {
-			return 'error';
-		}
-	}
-	*/
-
-	render() {
-		return (
-			<InputGroup>
-      			<FormControl
-					type="text"
-					bsSize="sm"
-					placeholder={this.props.placeholder}
-			 		onChange={this.handleChange}
-					defaultValue={this.props.value * 100} />
-				<InputGroup.Addon>%</InputGroup.Addon>
-			</InputGroup>
-		)
-	}
-}
-
 
 /*
 class LipidList extends Component {
